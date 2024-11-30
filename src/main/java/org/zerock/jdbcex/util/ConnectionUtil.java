32df270c01b61febd8d1e1348c070c4ed9d83a36
[GitHub <[GitHub>
2024-11-30 17:58:33 +0900

    면접 질문 생성 ë¯ 수정중

--- src/main/java/org/zerock/jdbcex/util/ConnectionUtil.java
+++ src/main/java/org/zerock/jdbcex/util/ConnectionUtil.java
@@ -12,9 +12,10 @@ public enum ConnectionUtil {
 
     INSTANCE, jdbc;
 
-    private static final String URL = "jdbc:mariadb://localhost:3306/merijob_db?useUnicode=true&characterEncoding=UTF-8";
+    private static final String URL = "jdbc:mariadb://localhost:3306/merijob_db?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+
     private static final String USER = "root";
-    private static final String PASSWORD = "1024";
+    private static final String PASSWORD = "1111";
     // ModelMapper instance
     private final ModelMapper modelMapper;
 
