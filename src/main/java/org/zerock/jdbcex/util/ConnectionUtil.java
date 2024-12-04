c7232adf047bac4ecc97ba0dd1a63e59cc9b019c
[GitHub <[GitHub>
2024-12-04 16:57:43 +0900

    이게맞나

--- src/main/java/org/zerock/jdbcex/util/ConnectionUtil.java
+++ src/main/java/org/zerock/jdbcex/util/ConnectionUtil.java
@@ -13,9 +13,9 @@ public enum ConnectionUtil {
 
     // Database connection details
 
-    private static final String URL = "jdbc:mariadb://localhost:3007/merijob_db";
+    private static final String URL = "jdbc:mariadb://localhost:3306/merijob_db?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
     private static final String USER = "root";
-    private static final String PASSWORD = "abcd980225*";
+    private static final String PASSWORD = "1111";
 
 
     // ModelMapper instance
