8d05d78a2fff7a9e5febf1ee28ccd239a30edadd
chaerin123 <hcr8540@naver.com>
2024-12-10 17:40:52 +0900

    sdsad

--- src/main/java/org/zerock/jdbcex/util/ConnectionUtil.java
+++ src/main/java/org/zerock/jdbcex/util/ConnectionUtil.java
@@ -15,9 +15,8 @@ public enum ConnectionUtil {
 
 
     private static final String URL = "jdbc:mariadb://localhost:3306/merijob_db?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
-
     private static final String USER = "root";
-    private static final String PASSWORD = "abcd980225*";
+    private static final String PASSWORD = "1024";
 
 
     // ModelMapper instance
